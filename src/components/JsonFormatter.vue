d95ff7d613788947c7e2344b4d8ab2f60f5c8fdb
Neo <qt_master@163.com>
2023-06-26 23:51:43 +0800

    save

--- src/components/JsonFormatter.vue
+++ src/components/JsonFormatter.vue
@@ -3,67 +3,67 @@
         <el-container>
             <el-header height="107px">
                 <el-container>
-      <el-aside width="200px" class="logo-area">JSON Editor</el-aside>
-      <el-main>
-    <el-menu
-    :default-active="activeIndex"
-    class="el-menu-demo"
-    mode="horizontal"
-  >
-    <el-menu-item index="1">About</el-menu-item>
-    <el-menu-item index="3">Introduce json</el-menu-item>
-  </el-menu></el-main>
-    </el-container>
-  
-</el-header>
+                    <el-aside width="200px" class="logo-area">JSON Editor</el-aside>
+                    <el-main>
+                        <el-menu :default-active="activeIndex" class="el-menu-demo" mode="horizontal">
+                            <el-menu-item index="1">About</el-menu-item>
+                            <el-menu-item index="2">Introduce json</el-menu-item>
+                            <el-menu-item index="3">Text Diff</el-menu-item>
+                            <el-menu-item index="3">Learn to code</el-menu-item>
+                        </el-menu>
+                    </el-main>
+                </el-container>
+
+            </el-header>
 
             <el-container>
                 <el-aside width="45%">
                     <div>
-                        <MonacoEditor @response="(payload)=>{json = payload}"/>
+                        <MonacoEditor @response="(payload) => { json = payload }" />
                     </div>
                 </el-aside>
                 <el-main class="json-preview-area">
                     <div>
-                        <vue-json-pretty :data="json_obj" :showLineNumber="true" :editable="true"/>
+                        <vue-json-pretty :data="json_obj" :showLineNumber="true" :editable="true" />
                     </div>
                 </el-main>
             </el-container>
         </el-container>
     </div>
-  </template>
+</template>
   
-  <script setup lang="ts">
-    import {ref,computed} from 'vue';
-    import VueJsonPretty from 'vue-json-pretty';
-    import MonacoEditor from './MonacoEditor.vue'
-    import 'vue-json-pretty/lib/styles.css';
-    let json = ref('')
-    const activeIndex = ref('1')
+<script setup lang="ts">
+import { ref, computed } from 'vue';
+import VueJsonPretty from 'vue-json-pretty';
+import MonacoEditor from './MonacoEditor.vue'
+import 'vue-json-pretty/lib/styles.css';
+let json = ref('')
+const activeIndex = ref('1')
 
-  const json_obj = computed(()=>{
-    if(json.value != ''){
-        try{
+const json_obj = computed(() => {
+    if (json.value != '') {
+        try {
             return JSON.parse(json.value)
-        }catch(e){
-           return {}     
+        } catch (e) {
+            return {}
         }
     }
     return {}
-    
-  })
- 
-  </script>
+
+})
+
+</script>
 
 <style scoped>
-    .json-preview-area{
-        overflow: scroll;
-        height:873px;
-        border:1px solid #626467;
-    }
-    .logo-area{
-        display: flex;
-        justify-content: center;
-        align-items: center;
-    }
+.json-preview-area {
+    overflow: scroll;
+    height: 873px;
+    border: 1px solid #626467;
+}
+
+.logo-area {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
 </style>
